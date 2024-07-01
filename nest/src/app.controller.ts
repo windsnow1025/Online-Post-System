430e18fc1c47821ebabeb4824c91ac1330a1c16a
windsnow1025 <windsnow1025@gmail.com>
2024-07-01 03:28:34 +0000

    nest basic

--- nest/src/app.controller.ts
+++ nest/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './common/decorators/public.decorator';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getRoot(): string {
+    return this.appService.getRoot();
   }
 }
