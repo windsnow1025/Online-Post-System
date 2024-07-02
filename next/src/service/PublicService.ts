be8d6752f0f099ce03ba1c5ed906a08c05701f72
windsnow1025 <windsnow1025@gmail.com>
2024-07-02 03:00:16 +0000

    chore(next): remove public service

--- next/src/service/PublicService.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default class PublicService {
-  async fetchMarkdown(filename: string): Promise<string> {
-    const response = await axios.get(`/markdown/${filename}`);
-    return response.data;
-  }
-}
